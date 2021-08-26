e348a2ab93eb3bde2de1bf2b9bd191a7a5639c6c
Jari-Pekka Teurajärvi <teurajarvi@hotmail.com>
2021-08-26 16:06:20 +0300

    Added some comments

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -23,30 +23,35 @@ export const signOut = () => {
 };
 
 // Action creators
+// POST
 export const createStream = (formValues) => async (dispacth) => {
   const response = await streams.post("/streams", formValues);
 
   dispacth({ type: CREATE_STREAM, payload: response.date });
 };
 
+// GET
 export const fetchStreams = () => async (dispacth) => {
   const response = await streams.get("/streams");
 
   dispacth({ type: FETCH_STREAMS, payload: response.data });
 };
 
+// GET
 export const fetchStream = (id) => async (dispacth) => {
   const response = await streams.get(`/streams/${id}`);
 
   dispacth({ type: FETCH_STREAM, payload: response.data });
 };
 
+// PUT
 export const editStream = (id, formValues) => async (dispacth) => {
   const response = await streams.put(`/streams/${id}`, formValues);
 
   dispacth({ type: EDIT_STREAM, payload: response.data });
 };
 
+// DELETE
 export const deleteStream = (id) => async (dispatch) => {
   await streams.delete(`/streams/${id}`);
 
