acb31ebf54dc91b6f6fa3288f483fb9e4f217144
Jari-Pekka Teurajärvi <teurajarvi@hotmail.com>
2021-08-31 14:45:14 +0300

    Implemented the modal button action Delete to delete the requested stream

--- client/src/components/streams/StreamDelete.js
+++ client/src/components/streams/StreamDelete.js
@@ -3,7 +3,7 @@ import { connect } from "react-redux";
 import { Link } from "react-router-dom";
 import Modal from "../Modal";
 import history from "../../history";
-import { fetchStream } from "../../actions";
+import { fetchStream, deleteStream } from "../../actions";
 
 class StreamDelete extends React.Component {
   componentDidMount() {
@@ -15,9 +15,14 @@ class StreamDelete extends React.Component {
       UI looking bad. React Fragment is like <></> but no
       Lint warnings */
   renderActions() {
+    const id = this.props.match.params.id;
     return (
       <React.Fragment>
-        <button className='ui button negative'>Delete</button>
+        <button
+          onClick={() => this.props.deleteStream(id)}
+          className='ui button negative'>
+          Delete
+        </button>
         <Link to='/' className='ui button'>
           Cancel
         </Link>
@@ -49,4 +54,6 @@ const mapStateToProps = (state, ownProps) => {
   return { stream: state.streams[ownProps.match.params.id] };
 };
 
-export default connect(mapStateToProps, { fetchStream })(StreamDelete);
+export default connect(mapStateToProps, { fetchStream, deleteStream })(
+  StreamDelete
+);
