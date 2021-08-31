e506dce20ccfd585b155e368edebc5246c2162cd
Jari-Pekka Teurajärvi <teurajarvi@hotmail.com>
2021-08-31 14:26:14 +0300

    Added fetchStream to the StreamDelete to get the info about the stream

--- client/src/components/streams/StreamDelete.js
+++ client/src/components/streams/StreamDelete.js
@@ -1,8 +1,14 @@
 import React from "react";
+import { connect } from "react-redux";
 import Modal from "../Modal";
 import history from "../../history";
+import { fetchStream } from "../../actions";
 
 class StreamDelete extends React.Component {
+  componentDidMount() {
+    this.props.fetchStream(this.props.match.params.id);
+  }
+
   /** Use React Fragment to enable two separate jsx functions
       in one go since if used extra <div> it will make the
       UI looking bad. React Fragment is like <></> but no
@@ -31,4 +37,4 @@ class StreamDelete extends React.Component {
   }
 }
 
-export default StreamDelete;
+export default connect(null, { fetchStream })(StreamDelete);
