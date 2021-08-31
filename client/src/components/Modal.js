b8ff53985b4139aaf2ee7a3a2925db107a4413f2
Jari-Pekka Teurajärvi <teurajarvi@hotmail.com>
2021-08-31 13:50:36 +0300

    Refactored the Modal to be re-usable

--- client/src/components/Modal.js
+++ client/src/components/Modal.js
@@ -1,10 +1,23 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import history from "../history";
 
 const Modal = (props) => {
   return ReactDOM.createPortal(
-    <div className='ui dimmer modals visible active'>
-      <div className='ui standard modal visible active'>This is Modal</div>
+    /* 1st div: Hide the modal if window or modal is clicked and 
+       forward to the homepage.
+       2nd div: Prevent hiding the modal if the modal itself is clicked 
+    */
+    <div
+      onClick={() => history.push("/")}
+      className='ui dimmer modals visible active'>
+      <div
+        onClick={(e) => e.stopPropagation()}
+        className='ui standard modal visible active'>
+        <div className='header'>{props.title}</div>
+        <div className='content'>{props.content}</div>
+        <div className='actions'>{props.actions}</div>
+      </div>
     </div>,
     document.querySelector("#modal")
   );
