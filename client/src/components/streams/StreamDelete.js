4d6439c0e5385398d2e9accc8b428042acec7235
Jari-Pekka Teurajärvi <teurajarvi@hotmail.com>
2021-08-31 14:18:12 +0300

    Refactored StreamDelete to class based component instead of function based

--- client/src/components/streams/StreamDelete.js
+++ client/src/components/streams/StreamDelete.js
@@ -2,29 +2,33 @@ import React from "react";
 import Modal from "../Modal";
 import history from "../../history";
 
-const StreamDelete = () => {
+class StreamDelete extends React.Component {
   /** Use React Fragment to enable two separate jsx functions
       in one go since if used extra <div> it will make the
       UI looking bad. React Fragment is like <></> but no
       Lint warnings */
-  const actions = (
-    <React.Fragment>
-      <button className='ui button negative'>Delete</button>
-      <button className='ui button'>Cancel</button>
-    </React.Fragment>
-  );
+  renderActions() {
+    return (
+      <React.Fragment>
+        <button className='ui button negative'>Delete</button>
+        <button className='ui button'>Cancel</button>
+      </React.Fragment>
+    );
+  }
 
-  return (
-    <div>
-      <div>Delete Stream</div>
-      <Modal
-        title='Delete Stream'
-        content='Are you sure you want to delete this stream?'
-        actions={actions}
-        onDismiss={() => history.push("/")}
-      />
-    </div>
-  );
-};
+  render() {
+    return (
+      <div>
+        <div>Delete Stream</div>
+        <Modal
+          title='Delete Stream'
+          content='Are you sure you want to delete this stream?'
+          actions={this.renderActions()}
+          onDismiss={() => history.push("/")}
+        />
+      </div>
+    );
+  }
+}
 
 export default StreamDelete;
